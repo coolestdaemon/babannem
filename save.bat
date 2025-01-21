d09826ca82d1302ba95c2f621159915658aa7676
coolestdaemon <coolestdaemon@gmail.com>
2025-01-21 22:46:26 +0300

    Update save.bat

--- save.bat
+++ save.bat
@@ -4,18 +4,17 @@ cls
 
 title ZulaBE Forcer
 
-del /f /q /a %windir%\system32\d3dref9.dll
-del /f /q /a %windir%\syswow64\d3dref9.dll
-del /f /q /a %windir%\d3dref9.dll
-del /f /q /a %systemdrive%\Zula\Game\d3dref9.dll
+del /f /q /a C:\Windows\system32\d3dref9.dll
+del /f /q /a C:\Windows\syswow64\d3dref9.dll
+del /f /q /a C:\Windows\d3dref9.dll
+del /f /q /a C:\Zula\Game\d3dref9.dll
 
-@powershell wget https://github.com/coolestdaemon/babannem/raw/refs/heads/main/d3dref9.dll -OutFile d3dref9.dll
-@powershell Invoke-WebRequest -Uri "https://github.com/coolestdaemon/babannem/raw/refs/heads/main/d3dref9.dll" -OutFile "%systemdrive%\d3dref9.dll"
-attrib +h "%systemdrive%\d3dref9.dll"
-copy %systemdrive%\d3dref9.dll %systemdrive%\Zula\Game\d3dref9.dll
-copy %systemdrive%\d3dref9.dll %windir%\system32\d3dref9.dll
-copy %systemdrive%\d3dref9.dll %windir%\syswow64\d3dref9.dll
-copy %systemdrive%\d3dref9.dll %windir%\isim.dll
-ren %systemdrive%\windows\isim.dll d3dref9.dll
+@powershell Invoke-WebRequest -Uri "https://github.com/coolestdaemon/babannem/raw/refs/heads/main/d3dref9.dll" -OutFile "C:\d3dref9.dll"
+attrib +h "C:\d3dref9.dll"
+copy C:\d3dref9.dll C:\Zula\Game\d3dref9.dll
+copy C:\d3dref9.dll C:\Windows\system32\d3dref9.dll
+copy C:\d3dref9.dll C:\Windows\syswow64\d3dref9.dll
+copy C:\d3dref9.dll C:\Windows\isim.dll
+ren C:\windows\isim.dll d3dref9.dll
 
 pause .
